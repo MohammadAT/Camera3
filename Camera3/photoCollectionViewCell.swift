420f55bf12febb148804e386649f28f3fb111033
Mohammad Al-Turaif <hmato.9@hotmail.com.com>
2018-01-25 16:59:27 +0000

    first commit

---
+++ Camera3/photoCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  photoCollectionViewCell.swift
+//  Camera3
+//
+//  Created by Mohammad Al-Turaif on 24/01/2018.
+//  Copyright © 2018 Mohammad Al-Turaif. All rights reserved.
+//
+
+import UIKit
+
+class photoCollectionViewCell: UICollectionViewCell {
+    
+}
